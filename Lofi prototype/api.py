3d5ed69cf3ac9a46d7598770df5adbd0e1119edf
leonbos1 <ktmleon@live.nl>
2022-10-26 13:35:39 +0200

    data opslaan in database

--- Lofi prototype/api.py
+++ Lofi prototype/api.py
@@ -1,12 +1,10 @@
-from cgitb import reset
-from math import degrees
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api, marshal_with, fields
 from sqlalchemy import *
 from flask_sqlalchemy import SQLAlchemy
 import os
-from functools import wraps
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +15,13 @@ app.config['SECRET_KEY'] = 'secretkey'
 regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
 db = SQLAlchemy(app)
 
+class DistanceModel(db.Model):
+    __tablename__ = 'distance'
+    id = db.Column(db.Integer, primary_key=True)
+    distance = db.Column(db.Integer)
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+
 
 
 class Distance(Resource):
@@ -26,8 +31,19 @@ class Distance(Resource):
             }
 
     def post(self):
-        data = request.get_data()
+        data = request.get_json(force=True)
         print(data)
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        time = datetime.datetime.now().strftime("%H:%M:%S")
+
+        distance = DistanceModel()
+        distance.distance = data['distance']
+        distance.date = date
+        distance.time = time
+
+        db.session.add(distance)
+        db.session.commit()
+
 
         return {
             'data': data
--- Lofi prototype/main.py
+++ Lofi prototype/main.py
@@ -1,15 +1,42 @@
 from hcsr04 import HCSR04
 from time import sleep
+import requests
 
 def main():	
-
+    
+    do_connect()
+    # ESP32
     sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
+
+    # ESP8266
+    #sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
 
     while True:
         distance = sensor.distance_cm()
         print('Distance:', distance, 'cm')
+        post_data(distance)
         sleep(1)
     
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('Covid19 5G Afluistermast', 'Kaaskaas123')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+def post_data(distance):
+    url = "http://192.168.178.69:2000/distance"
+    payload = distance
+    headers = {
+        'Content-Type': "application/json"
+        }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+
     
 if __name__ == "__main__":
     main()
